fe2152b486b6b10b221b917d01f3b725f135b3e0
Houssem <houssem-98@hotmail.com>
2017-10-12 09:40:01 +0200

    De offer en friends models afgemaakt.

---
+++ ProftaakASP/App_DAL/OfferSQLContext.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProftaakASP.Models;
+using System.Data.SqlClient;
+
+namespace ProftaakASP.App_DAL
+{
+    public class OfferSQLContext:IOfferContext
+    {
+        Offer offer;
+
+        public List<Offer> GetAllOffers()
+        {
+            string query = "SELECT * From Offer;";
+            List<Offer> OfferList = new List<Offer>();
+            using (SqlConnection conn = Database.Connection)
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        OfferList.Add(CreateOfferFromReader(dr));
+                    }
+                }
+                Database.Connection.Close();
+            }
+            return OfferList;
+        }
+
+        public Offer GetOfferInfo(int id)
+        {
+            string query = "SELECT * FROM Offer WHERE id = @id;";
+            using (SqlConnection conn = Database.Connection)
+            {
+                
+                SqlCommand cmd = new SqlCommand(query, conn);
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        offer = CreateOfferFromReader(dr);
+                    }
+                }
+            }
+            return offer;
+        }
+
+        public bool InsertOffer(Offer o)
+        {
+            string query = "INSERT INTO Offer(ID, AccountID, RequestID, Title, Context) VALUES (@id, @accountID, @requestID, @title, @context);";
+            using (SqlConnection conn = Database.Connection)
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", o.Id);
+                cmd.Parameters.AddWithValue("@accountID", o.AccountId);
+                cmd.Parameters.AddWithValue("@requestID", o.RequestId);
+                cmd.Parameters.AddWithValue("@title", o.Title);
+                cmd.Parameters.AddWithValue("@context", o.Context);
+
+                try
+                {
+                    if (Convert.ToInt32(cmd.ExecuteNonQuery()) > 0)
+                    {
+                        return true;
+                    }
+                }
+                catch (SqlException e)
+                {
+
+                }
+            }            
+            return false;
+        }
+
+        public bool DeleteOffer(int id)
+        {
+            string query = "DELETE FROM Offer WHERE Id = @id;";
+            using (SqlConnection conn = Database.Connection)
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            return false;
+        }
+
+        public Offer CreateOfferFromReader(SqlDataReader dr)
+        {
+            Offer o = new Offer(
+                Convert.ToInt32(dr["ID"]),
+                Convert.ToInt32(dr["AccountID"]),
+                Convert.ToInt32(dr["RequestID"]),
+                Convert.ToString(dr["Title"]),
+                Convert.ToString(dr["Context"]));
+            return o;
+        }
+    }
+}
---
+++ ProftaakASP/Controllers/OfferRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ProftaakASP.App_DAL;
+using ProftaakASP.Models;
+
+namespace ProftaakASP.Controllers
+{
+    public class OfferRepository
+    {
+        private IOfferContext context;
+
+        public OfferRepository(IOfferContext i)
+        {
+            this.context = i;
+        }
+
+        public List<Offer> GetAllOffers()
+        {
+            return context.GetAllOffers();
+        }
+
+        public bool InsertOffer(Offer o)
+        {
+            return context.InsertOffer(o);
+        }
+
+        public Offer GetOfferInfo(int id)
+        {
+            return context.GetOfferInfo(id);
+        }
+
+        public bool DeleteOffer(int id)
+        {
+            return context.DeleteOffer(id);
+        }
+    }
+}
